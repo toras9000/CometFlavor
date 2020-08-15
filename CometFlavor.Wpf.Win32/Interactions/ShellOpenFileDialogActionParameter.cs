9dd3cac6b5bbea47beb8848ca6938ab9da869181
toras9000 <toras9000@example.com>
2020-08-16 04:10:37 +0900

    シェルダイアログ用の呼び出しアクション追加

---
+++ CometFlavor.Wpf.Win32/Interactions/ShellOpenFileDialogActionParameter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CometFlavor.Wpf.Win32.Dialogs;
+
+namespace CometFlavor.Wpf.Win32.Interactions
+{
+    /// <summary>
+    /// <see cref="ShellOpenFileDialogAction"/> のパラメータ
+    /// </summary>
+    public class ShellOpenFileDialogActionParameter
+    {
+        #region 動作設定
+        /// <summary>ダイアログ表示パラメータ</summary>
+        public ShellOpenFileDialogParameter Parameter { get; set; }
+        #endregion
+
+        #region 結果
+        /// <summary>ダイアログの結果</summary>
+        public ShellOpenFileDialogResult Result { get; set; }
+
+        /// <summary>表示処理で発生した例外オブジェクト</summary>
+        public Exception Exception { get; set; }
+        #endregion
+    }
+}
---
+++ CometFlavor.Wpf.Win32/Interactions/ShellSaveFileDialogActionParameter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CometFlavor.Wpf.Win32.Dialogs;
+
+namespace CometFlavor.Wpf.Win32.Interactions
+{
+    /// <summary>
+    /// <see cref="ShellSaveFileDialogAction"/> のパラメータ
+    /// </summary>
+    public class ShellSaveFileDialogActionParameter
+    {
+        #region 動作設定
+        /// <summary>ダイアログ表示パラメータ</summary>
+        public ShellSaveFileDialogParameter Parameter { get; set; }
+        #endregion
+
+        #region 結果
+        /// <summary>ダイアログの結果</summary>
+        public ShellSaveFileDialogResult Result { get; set; }
+
+        /// <summary>表示処理で発生した例外オブジェクト</summary>
+        public Exception Exception { get; set; }
+        #endregion
+    }
+}
