4740d5d9d41f2fe8e8f4a5bc5450f95e747c670d
toras9000 <toras9000@example.com>
2020-09-03 23:31:11 +0900

    ビューのバリデーションエラー状態バインド用ビヘイビア

---
+++ CometFlavor.Wpf/Interactions/ViewValidationErrorBehavior.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Microsoft.Xaml.Behaviors;
+
+namespace CometFlavor.Wpf.Interactions
+{
+    /// <summary>
+    /// ビューの検証エラーをプロパティに表現するビヘイビア
+    /// </summary>
+    public class ViewValidationErrorBehavior : Behavior<DependencyObject>
+    {
+        // 構築
+        #region コンストラクタ
+        /// <summary>
+        /// デフォルトコンストラクタ
+        /// </summary>
+        public ViewValidationErrorBehavior()
+        {
+            this.ScanInitialState = true;
+        }
+        #endregion
+
+        // 公開プロパティ
+        #region 動作設定
+        /// <summary>要素へのアタッチ時にエラー状態を確認するか否か</summary>
+        public bool ScanInitialState { get; set; }
+        #endregion
+
+        #region 状態情報
+        /// <summary>ビューの検証エラー有無</summary>
+        /// <remarks>検証エラーが1つ以上あれば ture となる。</remarks>
+        public bool HasViewError
+        {
+            get { return (bool)GetValue(HasViewErrorProperty); }
+            set { SetValue(HasViewErrorProperty, value); }
+        }
+        #endregion
+
+        #region 依存プロパティ
+        /// <summary><see cref="HasViewError"/> の依存プロパティ</summary>
+        public static readonly DependencyProperty HasViewErrorProperty = DependencyProperty.Register(nameof(HasViewError), typeof(bool), typeof(ViewValidationErrorBehavior), new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, null, onHasViewErrorCoerceValue));
+        #endregion
+
+        // 保護メソッド
+        #region 配置
+        /// <summary>
+        /// 要素にアタッチした際の処理
+        /// </summary>
+        protected override void OnAttached()
+        {
+            // 基本クラス処理
+            base.OnAttached();
+
+            // 初期エラー状態を調べる設定の場合は要素を辿ってエラー状態を数える
+            if (this.ScanInitialState)
+            {
+                // 現時点のエラーの数を取得
+                // ちなみにValidation.GetErrors()で取得したエラーコレクションは一時的なものであるようで、
+                // 返されたインスタンスの中身が更新されることはなかったし、別のタイミングで再度取得すると返されるインスタンスは別であった。
+                this.errorCount = logicalDescendants(this.AssociatedObject).Select(e => Validation.GetErrors(e)?.Count ?? 0).Sum();
+            }
+
+            // エラーイベントのハンドラを登録
+            Validation.AddErrorHandler(this.AssociatedObject, onValidationError);
+
+            // エラーの数でプロパティ値を更新
+            this.HasViewError = 0 < this.errorCount;
+        }
+
+        /// <summary>
+        /// 要素からデタッチした際の処理
+        /// </summary>
+        protected override void OnDetaching()
+        {
+            // エラーイベントのハンドラを解除
+            Validation.RemoveErrorHandler(this.AssociatedObject, onValidationError);
+
+            // エラー情報をクリア
+            this.errorCount = 0;
+            this.HasViewError = false;
+
+            // 基本クラス処理
+            base.OnDetaching();
+        }
+        #endregion
+
+        #region エラー情報
+        /// <summary>現在のエラー数</summary>
+        private int errorCount;
+        #endregion
+
+        // 非公開メソッド
+        #region 依存プロパティのイベントハンドラ
+        /// <summary>
+        /// <see cref="HasViewError"/> 依存プロパティの値の矯正ハンドラ
+        /// </summary>
+        private static object onHasViewErrorCoerceValue(DependencyObject d, object baseValue)
+        {
+            // 一応自このクラス型であることを確認
+            if (d is ViewValidationErrorBehavior self)
+            {
+                // HasViewError はValidationエラーの状態をただ示したいので、どんな値を指定されようとも無視して把握しているエラー状態から値を作る。
+                return (0 < self.errorCount);
+            }
+
+            // もしも知らない型だったらそのままの値を返却しておく
+            return baseValue;
+        }
+        #endregion
+
+        #region 添付プロパティイベントハンドラ
+        /// <summary>
+        /// 検証エラーイベントハンドラ
+        /// </summary>
+        private void onValidationError(object sender, ValidationErrorEventArgs e)
+        {
+            // イベントの種類によって状態更新
+            switch (e.Action)
+            {
+                case ValidationErrorEventAction.Added:
+                    // エラーが増えた場合
+                    this.errorCount++;
+                    this.HasViewError = 0 < this.errorCount;
+                    break;
+
+                case ValidationErrorEventAction.Removed:
+                    // エラーが減った場合
+                    if (0 < this.errorCount)
+                    {
+                        this.errorCount--;
+                        this.HasViewError = 0 < this.errorCount;
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
+        #endregion
+
+        #region 補助処理
+        /// <summary>
+        /// 論理ツリー上の全ての子孫要素を列挙する
+        /// </summary>
+        /// <param name="element">起点となる要素</param>
+        /// <returns>論理ツリー上の要素を列挙するシーケンス</returns>
+        private IEnumerable<DependencyObject> logicalDescendants(DependencyObject element)
+        {
+            // 起点要素自体を列挙
+            yield return element;
+
+            // 子要素を列挙
+            foreach (var child in LogicalTreeHelper.GetChildren(element).OfType<DependencyObject>())
+            {
+                // 子要素とその子孫を列挙
+                foreach (var descendant in logicalDescendants(child))
+                {
+                    yield return descendant;
+                }
+            }
+        }
+        #endregion
+    }
+}
---
+++ Tests/TestCometFlavor.Wpf/Interactions/ViewValidationErrorBehaviorTests.cs
@@ -0,0 +1,407 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using CometFlavor.Wpf.Interactions;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Reactive.Bindings;
+using TestCometFlavor.Wpf._Test;
+
+namespace TestCometFlavor.Wpf.Interactions
+{
+    [TestClass]
+    public class ViewValidationErrorBehaviorTests
+    {
+        [TestMethod]
+        public void Test_Construct()
+        {
+            var target = new ViewValidationErrorBehavior();
+            target.HasViewError.Should().BeFalse();
+        }
+
+        [STATestMethod]
+        public void Test_HasViewError_BeforeAttach()
+        {
+            // テスト対象の準備
+            var target = new ViewValidationErrorBehavior();
+
+            // テスト対象をアタッチするコントロールを生成
+            var element = new TextBox();
+
+            // バインドソース
+            var rxp = new ReactiveProperty<int>();
+
+            // バインド
+            var binding = new Binding();
+            binding.Mode = BindingMode.TwoWay;
+            binding.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+            binding.Source = rxp;
+            binding.Path = new PropertyPath(nameof(rxp.Value));
+            binding.NotifyOnValidationError = true;
+            BindingOperations.SetBinding(element, TextBox.TextProperty, binding);
+
+            // パース出来ない値でエラーを発生させておく
+            element.Text = "asd";
+
+            // アタッチ
+            target.Attach(element);
+
+            // 事前のエラー状態が反映されること
+            target.HasViewError.Should().BeTrue();
+
+            // エラー無く更新可能な値
+            element.Text = "10";
+
+            // エラー状態がなくなること
+            target.HasViewError.Should().BeFalse();
+        }
+
+        [STATestMethod]
+        public void Test_HasViewError_BeforeAttach_NoInitScan()
+        {
+            // テスト対象の準備
+            var target = new ViewValidationErrorBehavior();
+            target.ScanInitialState = false;    // 初期エラー状態チェックなし
+
+            // テスト対象をアタッチするコントロールを生成
+            var element = new TextBox();
+
+            // バインドソース
+            var rxp = new ReactiveProperty<int>();
+
+            // バインド
+            var binding = new Binding();
+            binding.Mode = BindingMode.TwoWay;
+            binding.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+            binding.Source = rxp;
+            binding.Path = new PropertyPath(nameof(rxp.Value));
+            binding.NotifyOnValidationError = true;
+            BindingOperations.SetBinding(element, TextBox.TextProperty, binding);
+
+            // パース出来ない値でエラーを発生させておく
+            element.Text = "asd";
+
+            // アタッチ
+            target.Attach(element);
+
+            // 事前のエラー状態は考慮されない
+            target.HasViewError.Should().BeFalse();
+
+            // 別のパース不可エラー
+            element.Text = "def";
+
+            // エラーあり状態に変わりはないので変化しない
+            target.HasViewError.Should().BeFalse();
+
+            // エラー無く更新可能な値
+            element.Text = "10";
+
+            // もちろんエラーはなく
+            target.HasViewError.Should().BeFalse();
+
+            // 再度エラーとなる値にする
+            element.Text = "qwe";
+
+            // エラーあり状態となる
+            target.HasViewError.Should().BeTrue();
+        }
+
+        [STATestMethod]
+        public void Test_HasViewError_AfterAttach()
+        {
+            // テスト対象の準備
+            var target = new ViewValidationErrorBehavior();
+
+            // テスト対象をアタッチするコントロールを生成
+            var element = new TextBox();
+
+            // バインドソース
+            var rxp = new ReactiveProperty<int>();
+
+            // バインド
+            var binding = new Binding();
+            binding.Mode = BindingMode.TwoWay;
+            binding.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+            binding.Source = rxp;
+            binding.Path = new PropertyPath(nameof(rxp.Value));
+            binding.NotifyOnValidationError = true;
+            BindingOperations.SetBinding(element, TextBox.TextProperty, binding);
+
+            // アタッチ
+            target.Attach(element);
+
+            // 事前のエラーがない場合
+            target.HasViewError.Should().BeFalse();
+
+            // パース出来ない値でエラーを発生させる
+            element.Text = "asd";
+
+            // 発生したエラーを検出すること
+            target.HasViewError.Should().BeTrue();
+
+            // エラー無く更新可能な値
+            element.Text = "10";
+
+            // エラー状態がなくなること
+            target.HasViewError.Should().BeFalse();
+        }
+
+        [STATestMethod]
+        public void Test_HasViewError_BeforeAttach_SourceError()
+        {
+            // テスト対象の準備
+            var target = new ViewValidationErrorBehavior();
+
+            // テスト対象をアタッチするコントロールを生成
+            var element = new TextBox();
+
+            // バインドソース＆エラーソース
+            var errors = new List<string>();
+            var rxp = new ReactiveProperty<int>();
+            rxp.SetValidateNotifyError(v => errors);
+
+            // バインド
+            var binding = new Binding();
+            binding.Mode = BindingMode.TwoWay;
+            binding.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+            binding.Source = rxp;
+            binding.Path = new PropertyPath(nameof(rxp.Value));
+            binding.NotifyOnValidationError = true;
+            BindingOperations.SetBinding(element, TextBox.TextProperty, binding);
+
+            // ソース更新時に検証エラーありにする
+            errors.Add("tekito");
+
+            // ソースに到達する値
+            element.Text = "10";
+
+            // アタッチ
+            target.Attach(element);
+
+            // 発生したエラーを検出すること
+            target.HasViewError.Should().BeTrue();
+
+            // ソース更新時に検証エラーなしにする
+            errors.Clear();
+
+            // エラー無く更新可能な値
+            element.Text = "20";
+
+            // エラー状態がなくなること
+            target.HasViewError.Should().BeFalse();
+        }
+
+        [STATestMethod]
+        public void Test_HasViewError_BeforeAttach_SourceError_NoInitScan()
+        {
+            // テスト対象の準備
+            var target = new ViewValidationErrorBehavior();
+            target.ScanInitialState = false;    // 初期エラー状態チェックなし
+
+            // テスト対象をアタッチするコントロールを生成
+            var element = new TextBox();
+
+            // バインドソース＆エラーソース
+            var errors = new List<string>();
+            var rxp = new ReactiveProperty<int>();
+            rxp.SetValidateNotifyError(v => errors);
+
+            // バインド
+            var binding = new Binding();
+            binding.Mode = BindingMode.TwoWay;
+            binding.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+            binding.Source = rxp;
+            binding.Path = new PropertyPath(nameof(rxp.Value));
+            binding.NotifyOnValidationError = true;
+            BindingOperations.SetBinding(element, TextBox.TextProperty, binding);
+
+            // エラーありにする
+            errors.Add("tekito");
+            element.Text = "10";
+
+            // アタッチ
+            target.Attach(element);
+
+            // 事前のエラー状態は考慮されない
+            target.HasViewError.Should().BeFalse();
+
+            // 別のエラーありにする
+            errors.Add("hogehoge");
+            element.Text = "20";
+
+            // 違うエラーは見つけられる
+            target.HasViewError.Should().BeTrue();
+
+            // エラーなしにする
+            errors.Clear();
+            element.Text = "30";
+
+            // もちろんエラーはなく
+            target.HasViewError.Should().BeFalse();
+
+            // エラーありにする
+            errors.Add("tekito");
+            element.Text = "40";
+
+            // 一旦なくなった後なので検出できる
+            target.HasViewError.Should().BeTrue();
+
+        }
+
+        [STATestMethod]
+        public void Test_HasViewError_AfterAttach_SourceError()
+        {
+            // テスト対象の準備
+            var target = new ViewValidationErrorBehavior();
+
+            // テスト対象をアタッチするコントロールを生成
+            var element = new TextBox();
+
+            // バインドソース＆エラーソース
+            var errors = new List<string>();
+            var rxp = new ReactiveProperty<int>();
+            rxp.SetValidateNotifyError(v => errors);
+
+            // バインド
+            var binding = new Binding();
+            binding.Mode = BindingMode.TwoWay;
+            binding.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+            binding.Source = rxp;
+            binding.Path = new PropertyPath(nameof(rxp.Value));
+            binding.NotifyOnValidationError = true;
+            BindingOperations.SetBinding(element, TextBox.TextProperty, binding);
+
+            // アタッチ
+            target.Attach(element);
+
+            // 事前のエラーがない場合
+            target.HasViewError.Should().BeFalse();
+
+            // ソース更新時に検証エラーありにする
+            errors.Add("tekito");
+
+            // ソースに到達する値
+            element.Text = "10";
+
+            // 発生したエラーを検出すること
+            target.HasViewError.Should().BeTrue();
+
+            // ソース更新時に検証エラーなしにする
+            errors.Clear();
+
+            // エラー無く更新可能な値
+            element.Text = "20";
+
+            // エラー状態がなくなること
+            target.HasViewError.Should().BeFalse();
+        }
+
+        [STATestMethod]
+        public void Test_HasViewError_Multiple_BeforeAttach()
+        {
+            // テスト対象の準備
+            var target = new ViewValidationErrorBehavior();
+
+            // テスト対象をアタッチするコントロールを生成
+            var element = new StackPanel();
+            var textbox1 = new TextBox();
+            var textbox2 = new TextBox();
+            element.Children.Add(textbox1);
+            element.Children.Add(textbox2);
+
+            // テキストボックスにバインディング設定
+            void setBinding(TextBox textbox)
+            {
+                var rxp = new ReactiveProperty<int>();
+                var binding = new Binding();
+                binding.Mode = BindingMode.TwoWay;
+                binding.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+                binding.Source = rxp;
+                binding.Path = new PropertyPath(nameof(rxp.Value));
+                binding.NotifyOnValidationError = true;
+                BindingOperations.SetBinding(textbox, TextBox.TextProperty, binding);
+            }
+            setBinding(textbox1);
+            setBinding(textbox2);
+
+            // パース出来ない値でエラーを発生させておく
+            textbox1.Text = "asd";
+            textbox2.Text = "asd";
+
+            // アタッチ
+            target.Attach(element);
+
+            // 事前のエラー状態が反映されること
+            target.HasViewError.Should().BeTrue();
+
+            // 1つのエラー状態を消去
+            textbox1.Text = "100";
+
+            // まだエラー状態であること
+            target.HasViewError.Should().BeTrue();
+
+            // もう1つもエラー状態を消去
+            textbox2.Text = "200";
+
+            // エラー状態がなくなること
+            target.HasViewError.Should().BeFalse();
+        }
+
+        [STATestMethod]
+        public void Test_HasViewError_Multiple_AfterAttach()
+        {
+            // テスト対象の準備
+            var target = new ViewValidationErrorBehavior();
+
+            // テスト対象をアタッチするコントロールを生成
+            var element = new StackPanel();
+            var textbox1 = new TextBox();
+            var textbox2 = new TextBox();
+            element.Children.Add(textbox1);
+            element.Children.Add(textbox2);
+
+            // テキストボックスにバインディング設定
+            void setBinding(TextBox textbox)
+            {
+                var rxp = new ReactiveProperty<int>();
+                var binding = new Binding();
+                binding.Mode = BindingMode.TwoWay;
+                binding.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+                binding.Source = rxp;
+                binding.Path = new PropertyPath(nameof(rxp.Value));
+                binding.NotifyOnValidationError = true;
+                BindingOperations.SetBinding(textbox, TextBox.TextProperty, binding);
+            }
+            setBinding(textbox1);
+            setBinding(textbox2);
+
+            // アタッチ
+            target.Attach(element);
+
+            // 事前のエラー状態はなし
+            target.HasViewError.Should().BeFalse();
+
+            // パース出来ない値でエラーを発生させておく
+            textbox1.Text = "asd";
+            textbox2.Text = "asd";
+
+            // 事前のエラー状態が反映されること
+            target.HasViewError.Should().BeTrue();
+
+            // 1つのエラー状態を消去
+            textbox1.Text = "100";
+
+            // まだエラー状態であること
+            target.HasViewError.Should().BeTrue();
+
+            // もう1つもエラー状態を消去
+            textbox2.Text = "200";
+
+            // エラー状態がなくなること
+            target.HasViewError.Should().BeFalse();
+        }
+    }
+}
