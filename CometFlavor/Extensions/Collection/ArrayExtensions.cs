73a18dcb871bfd9d3eef16cf0db0bf81ad7f9650
toras9000 <toras9000@example.com>
2022-02-28 21:53:43 +0900

    メソッド追加

---
+++ CometFlavor/Extensions/Collection/ArrayExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace CometFlavor.Extensions.Collection;
+
+/// <summary>
+/// 配列 に対する拡張メソッド
+/// </summary>
+public static class ArrayExtensions
+{
+    /// <summary>
+    /// 配列のラッパー読み取り専用コレクションを作成する。
+    /// </summary>
+    /// <typeparam name="T">要素の型</typeparam>
+    /// <param name="self">対象配列</param>
+    /// <returns>読み取り専用コレクション</returns>
+    public static ReadOnlyCollection<T> AsReadOnly<T>(this T[] self)
+    {
+        return Array.AsReadOnly(self);
+    }
+}
---
+++ Tests/TestCometFlavor/Extensions/Collection/ArrayExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using CometFlavor.Extensions.Collection;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestCometFlavor.Extensions.Collection;
+
+[TestClass]
+public class ArrayExtensionsTests
+{
+    [TestMethod]
+    public void TestGetRelativeFile()
+    {
+        var source = new[] { 1, 2, 3, };
+        var wrapper = source.AsReadOnly();
+        wrapper.Should().Equal(1, 2, 3);
+
+        source[0] = 5;
+        wrapper.Should().Equal(5, 2, 3);
+    }
+
+}
