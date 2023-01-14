8a40bd6167d14b24d8015f07d4c32e8592bff15b
toras9000 <toras9000@example.com>
2023-01-15 00:56:31 +0900

    警告抑止

--- CometFlavor.Unicode/GraphemeClusterBreak.cs
+++ CometFlavor.Unicode/GraphemeClusterBreak.cs
@@ -1,23 +1,44 @@
 ﻿namespace CometFlavor.Unicode;
 
+/// <summary>
+/// GraphemeClusterBreak
+/// </summary>
 public enum GraphemeClusterBreak
 {
+    /// <summary>Other</summary>
     Other,
+    /// <summary>Prepend</summary>
     Prepend,
+    /// <summary>CR</summary>
     CR,
+    /// <summary>LF</summary>
     LF,
+    /// <summary>Control</summary>
     Control,
+    /// <summary>Extend</summary>
     Extend,
+    /// <summary>Regional_Indicator</summary>
     Regional_Indicator,
+    /// <summary>SpacingMark</summary>
     SpacingMark,
+    /// <summary>L</summary>
     L,
+    /// <summary>V</summary>
     V,
+    /// <summary>T</summary>
     T,
+    /// <summary>LV</summary>
     LV,
+    /// <summary>LVT</summary>
     LVT,
+    /// <summary>E_Base</summary>
     E_Base,
+    /// <summary>E_Modifier</summary>
     E_Modifier,
+    /// <summary>ZWJ</summary>
     ZWJ,
+    /// <summary>Glue_After_Zwj</summary>
     Glue_After_Zwj,
+    /// <summary>E_Base_GAZ</summary>
     E_Base_GAZ,
 }
