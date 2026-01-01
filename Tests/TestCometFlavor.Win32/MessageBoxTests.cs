042865fd752986eb8fe3155f42e6a96dd3dff954
toras9000 <toras9000@example.com>
2026-01-01 15:36:49 +0900

    テスト呼び出し

---
+++ Tests/TestCometFlavor.Win32/MessageBoxTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using AwesomeAssertions;
+using CometFlavor.Win32.Interaction;
+
+namespace TestCometFlavor.Win32;
+
+[TestClass]
+public sealed class MessageBoxTests
+{
+    [TestMethod]
+    public void Show()
+    {
+        if (!Debugger.IsAttached) Assert.Inconclusive("For debugging only");
+
+        var settings = new MessageBox.Settings(
+            Button: MessageBox.Button.YesNo,
+            Icon: MessageBox.Icon.Exclamation,
+            DefaultButton: MessageBox.DefaultButton.Button2,
+            Modality: MessageBox.Modality.System,
+            Options: MessageBox.Options.Foreground
+        );
+
+        var result = MessageBox.Show("message", "caption", settings);
+        result.Should().BeOneOf(MessageBox.Result.Yes, MessageBox.Result.No);
+    }
+}
---
+++ Tests/TestCometFlavor.Win32/ShellFileDialogTests.cs
@@ -0,0 +1,91 @@
+﻿using System.Diagnostics;
+using AwesomeAssertions;
+using CometFlavor.Win32.Dialogs;
+using Lestaly;
+
+namespace TestCometFlavor.Win32;
+
+[TestClass]
+public sealed class ShellFileDialogTests
+{
+    [TestMethod]
+    public void ShellOpenFileDialog()
+    {
+        if (!Debugger.IsAttached) Assert.Inconclusive("For debugging only");
+
+        var thisDir = ThisSource.Directory();
+
+        var parameters = new ShellOpenFileDialogParameter();
+        parameters.Title = "テストタイトル";
+        parameters.AcceptButtonLabel = "あくせぷと";
+        parameters.FileNameLabel = "ふぁいるのなまえ";
+        parameters.AdditionalPlaces.Add(new(thisDir.FullName, ShellFileDialogPlaceOrder.Top));
+        parameters.Directory = thisDir.FullName;
+        parameters.InitialFileName = "TestDefaultFileName";
+        parameters.DefaultExtension = "test";
+        parameters.Filters.Add(new("Test files", "*.test"));
+        parameters.Filters.Add(new("All files", "*"));
+        parameters.StrictFileTypes = false;
+        parameters.NoChangeDirectory = true;
+        parameters.PickFolders = false;
+        parameters.ForceFileSystem = false;
+        parameters.AllNonStorageItems = false;
+        parameters.NoValidate = false;
+        parameters.AllowMultiSelect = true;
+        parameters.PathMustExist = true;
+        parameters.FileMustExist = true;
+        parameters.CreatePrompt = false;
+        parameters.ShareAware = false;
+        parameters.NoReadOnlyReturn = false;
+        parameters.NoTestFileCreate = true;
+        parameters.HidePinnedPlaces = false;
+        parameters.NoDereferenceLinks = false;
+        parameters.OkButtonNeedsInteraction = false;
+        parameters.DontAddToRecent = true;
+        parameters.ForceShowHidden = false;
+        parameters.ForcePreviewPaneOn = false;
+
+        var dialog = new ShellOpenFileDialog();
+        var result = dialog.ShowDialog(parameters);
+        var noneOrFile = result.Item.IsWhiteSpace() || result.Item?.AsFileInfo().Exists == true;
+        noneOrFile.Should().BeTrue();
+    }
+
+    [TestMethod]
+    public void ShellSaveFileDialog()
+    {
+        if (!Debugger.IsAttached) Assert.Inconclusive("For debugging only");
+
+        var thisDir = ThisSource.Directory();
+
+        var parameters = new ShellSaveFileDialogParameter();
+        parameters.Title = "テストタイトル";
+        parameters.AcceptButtonLabel = "あくせぷと";
+        parameters.FileNameLabel = "ふぁいるのなまえ";
+        parameters.AdditionalPlaces.Add(new(thisDir.FullName, ShellFileDialogPlaceOrder.Top));
+        parameters.Directory = thisDir.FullName;
+        parameters.InitialFileName = "TestDefaultFileName";
+        parameters.DefaultExtension = "test";
+        parameters.Filters.Add(new("Test files", "*.test"));
+        parameters.Filters.Add(new("All files", "*"));
+        parameters.StrictFileTypes = false;
+        parameters.NoChangeDirectory = true;
+        parameters.ForceFileSystem = false;
+        parameters.AllNonStorageItems = false;
+        parameters.NoValidate = false;
+        parameters.PathMustExist = false;
+        parameters.FileMustExist = false;
+        parameters.CreatePrompt = false;
+        parameters.ShareAware = false;
+        parameters.NoReadOnlyReturn = false;
+        parameters.NoTestFileCreate = true;
+        parameters.HidePinnedPlaces = false;
+        parameters.NoDereferenceLinks = false;
+        parameters.OkButtonNeedsInteraction = false;
+        parameters.DontAddToRecent = true;
+        parameters.ForceShowHidden = false;
+
+        var dialog = new ShellSaveFileDialog();
+        dialog.ShowDialog(parameters);
+    }
+}
